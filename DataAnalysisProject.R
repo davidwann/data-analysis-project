5b7d74a9919eb7c70cbd5cbec254e1ffdb1023bb
David Wann <david.wann@gmail.com>
2016-06-08 22:22:30 -0500

    initial commit

---
+++ DataAnalysisProject.R
@@ -0,0 +1,45 @@
+
+# Final Project
+
+# DataAnalysisProject.R
+
+# getwd()
+# setwd("C:/Users/Joe/Google Drive/Coursera/Statistics with R/Introduction to Probability and Data/Final_Project")
+
+# brfss2013.RData
+
+# load(file, envir = parent.frame(), verbose = FALSE)
+
+# load("brfss2013.RData")
+
+library("dplyr")
+
+# help("library")
+# help("vector")
+
+# rm(the.crew)
+
+
+
+# the.crew <- data.frame(fname = c("Joe", "Bryan", "Helen"), 
+#                        age = c(34, 32, 33))
+
+
+# the.crew <- mutate(the.crew, marital.status = vector(mode = "character"))
+
+
+
+brfss2013$ssbsugar
+
+
+unique(brfss2013$ssbsugar)
+
+
+
+
+
+
+
+
+
+
