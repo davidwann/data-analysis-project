d3950dd950a9dce57766220a9b5612ca22eeeb58
unknown <david.wann@gmail.com>
2016-06-08 23:35:14 -0500

    commit from laptop machine

--- DataAnalysisProject.R
+++ DataAnalysisProject.R
@@ -5,12 +5,14 @@
 
 # getwd()
 # setwd("C:/Users/Joe/Google Drive/Coursera/Statistics with R/Introduction to Probability and Data/Final_Project")
+# setwd("C:\\Users\\David.David-PC\\Documents\\Coursera\\Introduction_to_Probability_and_Data\\Week_05_Data_Analysis_Project")
+
 
 # brfss2013.RData
 
 # load(file, envir = parent.frame(), verbose = FALSE)
 
-# load("brfss2013.RData")
+load("brfss2013.RData")
 
 library("dplyr")
 
@@ -18,7 +20,6 @@ library("dplyr")
 # help("vector")
 
 # rm(the.crew)
-
 
 
 # the.crew <- data.frame(fname = c("Joe", "Bryan", "Helen"), 
